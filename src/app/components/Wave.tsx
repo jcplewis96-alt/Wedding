6ded6f2abc117809293913000f716f8970728e3f
jcplewis96-alt <jcplewis96@gmail.com>
2026-04-09 22:41:57 -0500

    Add files via upload

---
+++ src/app/components/Wave.tsx
@@ -0,0 +1,35 @@
+interface WaveProps {
+  topColor: string;
+  bottomColor: string;
+  flip?: boolean;
+}
+
+export function Wave({ topColor, bottomColor, flip = false }: WaveProps) {
+  return (
+    <div style={{
+      position: "relative",
+      height: "80px",
+      marginTop: "-1px",
+      marginBottom: "-1px",
+      background: topColor,
+      overflow: "hidden",
+      flexShrink: 0,
+    }}>
+      <svg
+        viewBox="0 0 1440 80"
+        preserveAspectRatio="none"
+        style={{
+          position: "absolute",
+          bottom: 0, left: 0,
+          width: "100%", height: "100%",
+          transform: flip ? "scaleX(-1)" : "none",
+        }}
+      >
+        <path
+          d="M0,40 C180,80 360,0 540,40 C720,80 900,0 1080,40 C1260,80 1380,20 1440,40 L1440,80 L0,80 Z"
+          fill={bottomColor}
+        />
+      </svg>
+    </div>
+  );
+}
