1ebe42da0123a2419612a06f0e8116deb01bb4d8
jcplewis96-alt <jcplewis96@gmail.com>
2026-04-11 22:12:05 -0500

    Update Wave.tsx

--- src/app/components/Wave.tsx
+++ src/app/components/Wave.tsx
@@ -11,20 +11,24 @@ export function Wave({ topColor, bottomColor, flip = false }: WaveProps) {
       height: "80px",
       marginTop: "-2px",
       marginBottom: "-2px",
-      background: topColor,
       overflow: "hidden",
       flexShrink: 0,
+      display: "block",
     }}>
       <svg
         viewBox="0 0 1440 80"
         preserveAspectRatio="none"
         style={{
           position: "absolute",
-          bottom: 0, left: 0,
+          top: 0, left: 0,
           width: "100%", height: "100%",
+          display: "block",
           transform: flip ? "scaleX(-1)" : "none",
         }}
       >
+        {/* Top background rectangle */}
+        <rect x="0" y="0" width="1440" height="80" fill={topColor} />
+        {/* Wave shape in bottom color */}
         <path
           d="M0,40 C180,80 360,0 540,40 C720,80 900,0 1080,40 C1260,80 1380,20 1440,40 L1440,80 L0,80 Z"
           fill={bottomColor}
