265c8f7f35ddd971cb52ae8963177c88e87b9ee2
jcplewis96-alt <jcplewis96@gmail.com>
2026-04-12 16:21:42 -0500

    Update Wave.tsx

--- src/app/components/Wave.tsx
+++ src/app/components/Wave.tsx
@@ -1,35 +1,32 @@
 interface WaveProps {
-  topColor: string;
-  bottomColor: string;
+  color: string;
   flip?: boolean;
 }
 
-export function Wave({ topColor, bottomColor, flip = false }: WaveProps) {
+export function Wave({ color, flip = false }: WaveProps) {
   return (
     <div style={{
-      position: "relative",
-      marginTop: "-3px",
-      marginBottom: "-3px",
       lineHeight: 0,
-      flexShrink: 0,
-      display: "block",
       fontSize: 0,
+      display: "block",
+      overflow: "hidden",
+      marginTop: "-1px",
+      marginBottom: "-1px",
     }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 80"
+        viewBox="0 0 1440 60"
         preserveAspectRatio="none"
         style={{
           display: "block",
           width: "100%",
-          height: "80px",
-          transform: flip ? "scaleX(-1)" : "none",
+          height: "60px",
+          transform: flip ? "rotate(180deg)" : "none",
         }}
       >
-        <rect x="0" y="0" width="1440" height="80" fill={topColor} />
         <path
-          d="M0,40 C180,80 360,0 540,40 C720,80 900,0 1080,40 C1260,80 1380,20 1440,40 L1440,80 L0,80 Z"
-          fill={bottomColor}
+          d="M0,0 L0,30 C180,60 360,0 540,30 C720,60 900,0 1080,30 C1260,60 1380,15 1440,30 L1440,0 Z"
+          fill={color}
         />
       </svg>
     </div>
